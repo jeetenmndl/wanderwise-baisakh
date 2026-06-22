a64000f08637c38ac4a8c59557aadafebe03b87e
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-06-22 16:09:33 +0545

    Add registration form with validation and UI components

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,18 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import * as z from "zod"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Field, FieldError, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
 
 const formSchema = z.object({
     name: z.string().min(5, "Name should be at least 5 characters."),
@@ -25,8 +36,100 @@ const Register = () => {
         },
     })
 
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
-        <div>Register</div>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+            <Card className="w-1/3 mx-auto mt-32">
+                <CardHeader>
+                    <CardTitle>Register to WanderWise</CardTitle>
+                    <CardDescription>Fill your details to create an account</CardDescription>
+                    <CardAction>Card Action</CardAction>
+                </CardHeader>
+                <CardContent>
+
+                    <Controller
+                        name="name"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
+                                <Input
+                                    {...field}
+                                    id={field.name}
+                                    type="text"
+                                    placeholder="Ram Bahadur"
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                     <Controller
+                        name="email"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                <Input
+                                    {...field}
+                                    id={field.name}
+                                    type="email"
+                                    placeholder="ram.bahadur@example.com"
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                    
+                  <Controller
+                        name="password"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                                <Input
+                                    {...field}
+                                    id={field.name}
+                                    type="password"
+                                    placeholder="••••••••"
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                    <Controller
+                        name="confirmPassword"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={field.name}>Confirm Password</FieldLabel>
+                                <Input
+                                    {...field}
+                                    id={field.name}
+                                    type="password"
+                                    placeholder="••••••••"
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )}
+                    />
+
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+
+        </form>
     )
 }
 

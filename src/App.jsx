9616709407231b7f2db6e14d912cd3b3a1c74b9c
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-06-24 14:59:03 +0545

    Add login functionality with form validation and API integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import About from './pages/About'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
 
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )

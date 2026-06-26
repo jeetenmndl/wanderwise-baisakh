32405b385a7ae3061dcfb8abf759e9468a2d3ee4
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-06-26 15:09:26 +0545

    Add Toaster component for enhanced notification display

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <ThemeProvider>
       <AuthProvider>
+
+        <Toaster richColors position='top-right' />
         <App />
       </AuthProvider>
     </ThemeProvider>
